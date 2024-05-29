2de98b91a061a227a432060c7256656c9cfff336
Gabriel Reus Rodriguez <gabriel.reusrodriguez@gmail.com>
2024-05-29 13:01:33 +0200

    Pruebas y correcciones

--- experimental/src/builtin_echo.c
+++ experimental/src/builtin_echo.c
@@ -6,7 +6,7 @@
 /*   By: greus-ro <greus-ro@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/03 12:21:39 by abluis-m          #+#    #+#             */
-/*   Updated: 2024/05/29 07:52:51 by greus-ro         ###   ########.fr       */
+/*   Updated: 2024/05/29 12:59:24 by greus-ro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,7 @@ bool	check_nflag(t_cmd cmd)
 	return (false);
 }
 
+/*
 int	builtin_echo(t_cmd cmd)
 {
 	t_list	*node;
@@ -34,7 +35,7 @@ int	builtin_echo(t_cmd cmd)
 	bool	nflag;
 	int		out;
 
-	out = STDOUT_FILENO;
+//	out = STDOUT_FILENO;
 	nflag = check_nflag(cmd);
 	node = cmd.args;
 	if (nflag)
@@ -42,12 +43,41 @@ int	builtin_echo(t_cmd cmd)
 	while (node != NULL)
 	{
 		token = (t_token *)node->content;
-		ft_putstr_fd(token->value, out);
-		if (node->next)
-			ft_putchar_fd(' ', out);
+		if (ft_strcmp("-n",token->value) != 0)
+		{
+			ft_putstr_fd(token->value, out);
+			if (node->next)
+				ft_putchar_fd(' ', out);
+		}
 		node = node->next;
 	}
 	if (!nflag)
 		ft_putchar_fd('\n', out);
+	return (EXIT_SUCCESS);
+}
+*/
+int	builtin_echo(t_cmd cmd)
+{
+	t_list	*node;
+	t_token	*token;
+	bool	nflag;
+
+	nflag = check_nflag(cmd);
+	node = cmd.args;
+	if (nflag)
+		node = node->next;
+	while (node != NULL)
+	{
+		token = (t_token *)node->content;
+		if (ft_strcmp("-n",token->value) != 0)
+		{
+			ft_putstr_fd(token->value, STDOUT_FILENO);
+			if (node->next)
+				ft_putchar_fd(' ', STDOUT_FILENO);
+		}
+		node = node->next;
+	}
+	if (!nflag)
+		ft_putchar_fd('\n', STDOUT_FILENO);
 	return (EXIT_SUCCESS);
 }
